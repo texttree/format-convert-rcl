12324b77eae762195b27be37efc18894957e6bcb
Pavel <braurs64@gmail.com>
2023-05-23 21:48:34 +0400

    perf: :zap: added try/catch

--- src/components/JsonToPdf/JsonToPdf.js
+++ src/components/JsonToPdf/JsonToPdf.js
@@ -18,16 +18,21 @@ function JsonToPdf({ data, styles, filename }) {
     };
 
     const getImageDataUrl = async (url) => {
-      const response = await fetch(url);
-      const blob = await response.blob();
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          resolve(reader.result);
-        };
-        reader.onerror = reject;
-        reader.readAsDataURL(blob);
-      });
+      try {
+        const response = await fetch(url);
+        const blob = await response.blob();
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            resolve(reader.result);
+          };
+          reader.onerror = reject;
+          reader.readAsDataURL(blob);
+        });
+      } catch (error) {
+        console.error('Error fetching image data URL:', error);
+        throw new Error('Invalid URL');
+      }
     };
 
     const addVerseObjects = async () => {
